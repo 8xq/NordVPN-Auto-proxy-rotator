bfe37b16aabf51d5e153ab79e53ffb0d702539d9
HDzzzz <40918889+HDzzzz@users.noreply.github.com>
2021-01-04 19:28:27 +0000

    Add files via upload

---
+++ C#/NordVPN-auto-switcher/Servers/RandomCountry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NordVPN_auto_switcher.Servers
+{
+    class RandomCountry
+    {
+        List<string> countryList = System.IO.File.ReadLines(@"CountryList.txt").ToList();
+        public string GrabRandomCountry()
+        {
+            return countryList.OrderBy(x => Guid.NewGuid()).FirstOrDefault();
+        }
+    }
+}
