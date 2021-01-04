d1f0b6497d38cc0bfa88999b435be8b4a32d60cb
HDzzzz <40918889+HDzzzz@users.noreply.github.com>
2021-01-04 19:25:30 +0000

    Add files via upload

---
+++ C#/NordVPN-auto-switcher/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace NordVPN_auto_switcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroComboBox1 = new MetroSuite.MetroComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.metroButton2 = new MetroSuite.MetroButton();
+            this.metroButton1 = new MetroSuite.MetroButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.metroTextbox1 = new MetroSuite.MetroTextbox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.metroControlBox1 = new MetroSuite.MetroControlBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.AccentColor = System.Drawing.Color.Indigo;
+            this.metroComboBox1.ArrowColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(98)))), ((int)(((byte)(98)))));
+            this.metroComboBox1.AutoStyle = false;
+            this.metroComboBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(98)))), ((int)(((byte)(98)))));
+            this.metroComboBox1.DefaultColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.metroComboBox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(250)))), ((int)(((byte)(250)))));
+            this.metroComboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.metroComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.metroComboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.metroComboBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.metroComboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.Items.AddRange(new object[] {
+            "Random (country)",
+            "P2P"});
+            this.metroComboBox1.Location = new System.Drawing.Point(9, 37);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.metroComboBox1.Size = new System.Drawing.Size(255, 24);
+            this.metroComboBox1.Style = MetroSuite.Design.Style.Custom;
+            this.metroComboBox1.TabIndex = 0;
+            this.metroComboBox1.TextAlignment = System.Drawing.StringAlignment.Center;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.metroButton2);
+            this.groupBox1.Controls.Add(this.metroButton1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.metroTextbox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.metroComboBox1);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(12, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(270, 172);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.AutoStyle = false;
+            this.metroButton2.BackColor = System.Drawing.Color.Transparent;
+            this.metroButton2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.metroButton2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(98)))), ((int)(((byte)(98)))));
+            this.metroButton2.DefaultColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.metroButton2.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.metroButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.metroButton2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.metroButton2.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(63)))));
+            this.metroButton2.Location = new System.Drawing.Point(6, 140);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.PressedColor = System.Drawing.Color.Indigo;
+            this.metroButton2.RoundingArc = 23;
+            this.metroButton2.Size = new System.Drawing.Size(255, 23);
+            this.metroButton2.Style = MetroSuite.Design.Style.Custom;
+            this.metroButton2.TabIndex = 6;
+            this.metroButton2.Text = "stop & disconnect";
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.AutoStyle = false;
+            this.metroButton1.BackColor = System.Drawing.Color.Transparent;
+            this.metroButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.metroButton1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(98)))), ((int)(((byte)(98)))));
+            this.metroButton1.DefaultColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.metroButton1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.metroButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.metroButton1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.metroButton1.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(63)))), ((int)(((byte)(63)))));
+            this.metroButton1.Location = new System.Drawing.Point(6, 111);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.PressedColor = System.Drawing.Color.Indigo;
+            this.metroButton1.RoundingArc = 23;
+            this.metroButton1.Size = new System.Drawing.Size(255, 23);
+            this.metroButton1.Style = MetroSuite.Design.Style.Custom;
+            this.metroButton1.TabIndex = 5;
+            this.metroButton1.Text = "start & connect";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "how often to grab a new IP (seconds)";
+            // 
+            // metroTextbox1
+            // 
+            this.metroTextbox1.AutoStyle = false;
+            this.metroTextbox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.metroTextbox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(98)))), ((int)(((byte)(98)))), ((int)(((byte)(98)))));
+            this.metroTextbox1.DefaultColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.metroTextbox1.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.metroTextbox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.metroTextbox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(153)))), ((int)(((byte)(153)))));
+            this.metroTextbox1.HideSelection = false;
+            this.metroTextbox1.HoverColor = System.Drawing.Color.Indigo;
+            this.metroTextbox1.Location = new System.Drawing.Point(6, 82);
+            this.metroTextbox1.Name = "metroTextbox1";
+            this.metroTextbox1.PasswordChar = '\0';
+            this.metroTextbox1.Size = new System.Drawing.Size(255, 23);
+            this.metroTextbox1.Style = MetroSuite.Design.Style.Custom;
+            this.metroTextbox1.TabIndex = 3;
+            this.metroTextbox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "server to connect:";
+            // 
+            // metroControlBox1
+            // 
+            this.metroControlBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.metroControlBox1.Location = new System.Drawing.Point(195, 3);
+            this.metroControlBox1.Name = "metroControlBox1";
+            this.metroControlBox1.Size = new System.Drawing.Size(96, 32);
+            this.metroControlBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "status:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "idle";
+            // 
+            // Form1
+            // 
+            this.AccentColor = System.Drawing.Color.Indigo;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 239);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.metroControlBox1);
+            this.Controls.Add(this.groupBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Name = "Form1";
+            this.State = MetroSuite.MetroForm.FormState.Custom;
+            this.Style = MetroSuite.Design.Style.Dark;
+            this.Text = "NordVPN changer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroSuite.MetroComboBox metroComboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private MetroSuite.MetroControlBox metroControlBox1;
+        private MetroSuite.MetroButton metroButton2;
+        private MetroSuite.MetroButton metroButton1;
+        private System.Windows.Forms.Label label2;
+        private MetroSuite.MetroTextbox metroTextbox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ C#/NordVPN-auto-switcher/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Leaf.xNet;
+namespace NordVPN_auto_switcher
+{
+    public partial class Form1 : MetroSuite.MetroForm
+    {
+        Servers.RandomCountry SL = new Servers.RandomCountry();
+        bool running = false;
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        // The method to change proxy via batch command (hidden console)
+        private void ChangeIP(string server , int delay)
+        {
+            while (running == true)
+            {
+                string command = "";
+                switch (server.ToLower())
+                {
+                    case "p2p":
+                        command = "nordvpn -c --group-name \"p2p\"";
+                        label4.Text = "Connecting via peer 2 peer proxy";
+                        break;
+                    case "random (country)":
+                        string server_use = SL.GrabRandomCountry();
+                        label4.Text = "Grabbing server from " + server_use;
+                        command = "nordvpn -c --group-name " + server_use;
+                        break;
+                    default:
+                        MessageBox.Show("Error - please make sure a server is selected !", "Error - invalid option");
+                        break;
+                }
+
+                Process CMD_exec = new Process();
+                CMD_exec.StartInfo.FileName = "cmd";
+                CMD_exec.StartInfo.Arguments = "/c cd \"C:\\Program Files\\NordVPN\" & " + command + " \\";
+                CMD_exec.StartInfo.CreateNoWindow = true;
+                CMD_exec.StartInfo.UseShellExecute = false;
+                CMD_exec.Start();
+                CMD_exec.WaitForExit();
+                label4.Text = "Changing IP -/- setting new IP";
+                Thread.Sleep(delay * 1000);
+            }
+        }
+        // Start button 
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            string server = metroComboBox1.Text;
+            string delay = metroTextbox1.Text;
+            bool CheckDelay = string.IsNullOrEmpty(delay);
+            bool CheckServer = string.IsNullOrEmpty(server);
+            if(CheckServer == false && CheckDelay == false)
+            {
+                running = true;
+                Thread changer = new Thread(() => ChangeIP(server , Convert.ToInt32(delay)));
+                changer.Start();
+            }
+            else
+            {
+                MessageBox.Show("Error - please check all fields are filled in !", "Error - missing infomration");
+            }
+        }
+
+        // Stop button 
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            running = false;
+            label4.Text = "Stopping";
+            Process CMD_exec = new Process();
+            CMD_exec.StartInfo.FileName = "cmd";
+            CMD_exec.StartInfo.Arguments = "/c cd \"C:\\Program Files\\NordVPN\" & nordvpn --disconnect \\";
+            CMD_exec.StartInfo.CreateNoWindow = true;
+            CMD_exec.StartInfo.UseShellExecute = false;
+            CMD_exec.Start();
+            CMD_exec.WaitForExit();
+            label4.Text = "idle";
+        }
+    }
+}
